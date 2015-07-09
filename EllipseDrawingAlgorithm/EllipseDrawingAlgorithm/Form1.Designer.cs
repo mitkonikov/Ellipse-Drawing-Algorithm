655a69ff89fdcd40fc8cdad5f68751678ef76f19
Mitko Nikov <mitkonikov01@gmail.com>
2015-07-09 13:17:15 +0200

    initial commit

---
+++ EllipseDrawingAlgorithm/EllipseDrawingAlgorithm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EllipseDrawingAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RotationTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 362);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RotationTimer
+            // 
+            this.RotationTimer.Interval = 20;
+            this.RotationTimer.Tick += new System.EventHandler(this.RotationTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 362);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ellipse Drawing by Angle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer RotationTimer;
+    }
+}
+
---
+++ EllipseDrawingAlgorithm/EllipseDrawingAlgorithm/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EllipseDrawingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp = new Bitmap(1500, 1500);
+        Graphics g;
+        // Settings
+        int radiusX = 1500 / 2 - 120;
+        int radiusY = 1500 / 2 - 370;
+        int center = 1500 / 2;
+        int angle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp); //Binding the graphics to the Bitmap
+            pictureBox1.Image = bmp;
+            RotationTimer.Start();
+        }
+
+        private Point EllipseDrawing(int dHalfwidthEllipse, int dHalfheightEllipse, Point origin, int t)
+        {
+            Point ptfPoint = new Point((int)(origin.X + dHalfwidthEllipse * Math.Cos(t * Math.PI / 180)), (int)(origin.Y + dHalfheightEllipse * Math.Sin(t * Math.PI / 180)));
+            return ptfPoint;
+        }
+
+        private void RotationTimer_Tick(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bmp); //Binding the graphics to the Bitmap
+
+            // g.Clear(Color.White); If you want to clear out the drawing on the previous angle
+
+            // Draw the ellipse
+            int x = EllipseDrawing(radiusX, radiusY, new Point(center, center), angle).X; // Find the X Coords
+            int y = EllipseDrawing(radiusX, radiusY, new Point(center, center), angle).Y; // Find the Y Coords
+            g.FillRectangle(new SolidBrush(Color.DarkBlue), x, y, 10, 10); // Draw rectangles as pixels
+            // g.DrawLine(new Pen(Color.DarkBlue, 4f), new Point(center, center), new Point(x, y)); If you want to draw a line from the origin to the point on the ellipse
+
+            pictureBox1.Image = bmp;
+            g.Dispose();
+
+            if (angle == 360)
+            {
+                RotationTimer.Stop();
+            }
+            else
+            {
+                angle++;
+            }
+        }
+    }
+}
